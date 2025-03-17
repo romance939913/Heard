f8b35c24ae8200094b24feae4fb21cdbc3f721a6
Brennan Romance <brennanromance@Brennans-iMac.local>
2025-03-16 23:03:37 -0400

    add more routes and react client

--- flask-server/app.py
+++ flask-server/app.py
@@ -2,18 +2,20 @@ from flask import Flask, request, render_template, jsonify
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from config import Config
 import os
 import psycopg2
 
 # setup Flask app
 app = Flask(__name__)
+CORS(app)
 
 # setup password encryption
 bcrypt = Bcrypt(app)
 
 # setup JWT user authorization
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:{Config.POSTGRESQL_PASSWORD}@localhost/heard"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://brennanromance:{Config.POSTGRESQL_PASSWORD}@localhost/heard"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this in production
 
@@ -51,17 +53,39 @@ class Suggestion(db.Model):
     upvotes = db.Column(db.Integer, nullable=False)
     downvotes = db.Column(db.Integer, nullable=False)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'user_id': self.user_id,
+            'company_id': self.company_id,
+            'product_id': self.product_id,
+            'upvotes': self.upvotes,
+            'downvotes': self.downvotes
+        }
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     company_id = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'company_id': self.company_id,
+        }
+    
 
 # Create the database tables (run this only once)
 with app.app_context():
     db.create_all()
 
+
+# company routes
 @app.post("/api/company")
 @jwt_required()
 def create_company():
@@ -86,17 +110,48 @@ def get_companies():
     return jsonify([company.to_dict() for company in companies]), 201
 
 
+# product routes
+@app.post("/api/product")
+@jwt_required()
+def create_product():
+    data = request.get_json()
+    name = data["name"]
+    company_id = data["company_id"]
+    description = data["description"]
+
+    if not data or not data.get('name') or not data.get('description') or not data.get('company_id'):
+        return jsonify({"msg": "Missing required fields"}), 400
+
+    new_product = Product(name=name, company_id=company_id, description=description)
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify(name=name, company_id=company_id, description=description), 201
+
+
+@app.get("/api/product")
+@jwt_required()
+def get_products():
+    data = request.get_json()
+    
+    if not data or not data.get("company_id"):
+        return jsonify({"msg": "Missing required fields: company_id"}), 400
+    
+    company_id = data["company_id"]
+    
+    products = Product.query.filter_by(company_id=company_id)
+    return jsonify([product.to_dict() for product in products]), 201
+
+
+# suggestion routes
 @app.post("/api/suggestion")
 @jwt_required()
 def create_suggestion():
     data = request.get_json()
     
-    if not data or not data.get("title") or not data.get("user_id") or not data.get("company_id"):
+    if not data or not data.get("title") or not data.get("user_id") or not data.get("company_id") or not data.get("description") or not data.get("product_id"):
         return jsonify({"msg": "Missing required fields"}), 400
     
     title, description, user_id, company_id, product_id = data["title"], data["description"], data["user_id"], data["company_id"], data["product_id"]
-    if not product_id:
-        product_id = None
 
     suggestion = Suggestion.query.filter_by(title=title).first()
     if suggestion:
@@ -104,11 +159,31 @@ def create_suggestion():
 
     new_suggestion = Suggestion(title=title, description=description, user_id=user_id, company_id=company_id, product_id=product_id, upvotes=0, downvotes=0)
     db.session.add(new_suggestion)
-    db.session.commit(title=title, description=description, user_id=user_id, company_id=company_id, product_id=product_id, upvotes=0, downvotes=0)
+    db.session.commit()
 
-    return jsonify({ "msg": "suggess" }), 200
+    return jsonify(title=title, description=description, user_id=user_id, company_id=company_id, product_id=product_id, upvotes=0, downvotes=0), 200
 
 
+@app.get("/api/suggestion")
+@jwt_required()
+def get_suggestions():
+    data = request.get_json()
+
+    if not data or not data.get("product_id_included") or not data.get("filter_id"):
+        return jsonify({"msg": "Missing required fields"}), 400
+    
+    product_id_included = data["product_id_included"]
+    filter_id = data['filter_id']
+
+    suggestions = None
+    if product_id_included:
+        suggestions = Suggestion.query.filter_by(product_id=filter_id)
+    else:
+        suggestions = Suggestion.query.filter_by(company_id=filter_id)
+    return jsonify([suggestion.to_dict() for suggestion in suggestions]), 201
+
+
+# user auth routes
 @app.route("/api/register", methods=["POST"])
 def register():
     data = request.get_json()
