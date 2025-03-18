ae4d1b74df4025887d720f1221c9a42fcab2b45c
Brennan Romance <brennanromance@Brennans-iMac.local>
2025-03-18 10:38:40 -0400

    Add starting react routes

---
+++ client/src/components/feed/feed.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function Feed() {
+  return (
+    <div>
+        <h1>Feed</h1>
+    </div>
+  )
+}
+
+export default Feed
---
+++ client/src/components/userAuth/signin.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function Signin() {
+  return (
+    <div>
+        <h1>This is the Signin page</h1>
+    </div>
+  )
+}
+
+export default Signin
